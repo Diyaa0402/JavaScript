8d3ea2b3f0192ef1db29d8207415f28c78cdd937
Diyaa0402 <123350612+Diyaa0402@users.noreply.github.com>
2024-01-22 09:23:56 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144556  // constant -> we can never change
+let accEmail = "diam@gmail.com" 
+var accPass ="12345"
+accountCity ="jaipur";
+
+console.log(accountId);
+
+/** dont use var data type becoz of the block scope issue */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+//"use strict"; // treat all JS code as newer version
+
+//alert(3+3)  -> only browser have this feature
+
+// number -> 2 to power 53
+//bigint
+// string -> ""
+//boolean -> true/ flase
+//null -> standalone value , null ek object hai
+//undefined -> abhi value assign nhi hui hai
+// symbol -> unique
+
+//object
+
+console.log(typeof null);
